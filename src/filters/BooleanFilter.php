9c347343e79d397e1c892d11d536cc72f5baa59d
Murat Çelik <muratcelik.27@outlook.com>
2017-05-19 20:06:57 +0300

    BooleanFilter

---
+++ src/filters/BooleanFilter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace advancedfilter\src\filters;
+
+use advancedfilter\src\base\Filter;
+use yii\helpers\Html;
+
+/**
+ * Description of BooleanFilter
+ *
+ * @author Murat Çelik
+ */
+class BooleanFilter extends Filter
+{
+
+    public function drawFilter() {
+        return Html::activeCheckbox($this->model, $this->attribute, $this->options);
+    }
+
+    public function executeFilter() {
+        return $this->activeQuery->andFilterCompare($this->attribute, $this->model->{$this->attribute});
+    }
+
+}
